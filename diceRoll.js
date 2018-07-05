63f83c2266a8a8e2a99e96df023c10406cbf093a
Michael Agard <oraclekai@gmail.com>
2018-07-05 17:09:06 -0400

    added comment in diceRoll.js explaining the issue

--- diceRoll.js
+++ diceRoll.js
@@ -1,9 +1,6 @@
 function diceRoll() {
     let minRoll = document.getElementById("minRoll").value;
     let maxRoll = document.getElementById("maxRoll").value;
-    let roll = Math.floor(Math.random() * (maxRoll - minRoll + 1)) + minRoll;
-    document.getElementById("diceOutput").innerHTML = roll;
+    let test1 = Math.floor(Math.random() * (maxRoll - minRoll + 1)) + minRoll;
+    document.getElementById("diceOutput").innerHTML = test1; // HELP! This doesn't work. EG: 1/15 returns a random number between 1 and 150.
 }
-//function diceRoll() {
-//   document.getElementById("diceOutput").innerHTML = "Paragraph changed.";
-//}
