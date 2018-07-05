f3bc84fcf31d4e10cb510a24fa8db97419d39255
Michael Agard <oraclekai@gmail.com>
2018-07-05 18:31:23 -0400

    added css to remove spin button, added fix for javascript passing string and not a number, turned off autocomplete

--- diceRoll.js
+++ diceRoll.js
@@ -1,6 +1,6 @@
 function diceRoll() {
-    let minRoll = document.getElementById("minRoll").value;
-    let maxRoll = document.getElementById("maxRoll").value;
-    let test1 = Math.floor(Math.random() * (maxRoll - minRoll + 1)) + minRoll;
-    document.getElementById("diceOutput").innerHTML = test1; // HELP! This doesn't work. EG: 1/15 returns a random number between 1 and 150.
+    let minRoll = Number(document.getElementById("minRoll").value);
+    let maxRoll = Number(document.getElementById("maxRoll").value);
+    let roll = Math.floor(Math.random() * (maxRoll - minRoll + 1)) + minRoll;
+    document.getElementById("diceOutput").innerHTML = roll; // HELP! This doesn't work. EG: 1/15 returns a random number between 1 and 150.
 }
