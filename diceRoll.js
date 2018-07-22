ad1dae0664af1e6635a55f21f223031d564581f1
Michael Agard <oraclekai@gmail.com>
2018-07-22 19:52:39 -0400

    Added error functionality for blank fields. Cleaned up JS a bit.

--- diceRoll.js
+++ diceRoll.js
@@ -1,6 +1,10 @@
 function diceRoll() {
-    let minRoll = Number(document.getElementById("minRoll").value);
-    let maxRoll = Number(document.getElementById("maxRoll").value);
-    let roll = Math.floor(Math.random() * (maxRoll - minRoll + 1)) + minRoll;
-    document.getElementById("diceOutput").innerHTML = roll; // HELP! This doesn't work. EG: 1/15 returns a random number between 1 and 150.
+  let minRoll = Number(document.getElementById("minRoll").value);
+  let maxRoll = Number(document.getElementById("maxRoll").value);
+  let roll = Math.floor(Math.random() * (maxRoll - minRoll + 1)) + minRoll;
+  if (minRoll && maxRoll) {
+    return document.getElementById("diceOutput").innerHTML = roll;
+  } else {
+    return document.getElementById("diceOutput").innerHTML = "Error: Blank fields";
+  }
 }
